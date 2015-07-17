e85353f8dd0219ca6a5b1a31a15934c67463ef45
ansopa <angel.sobrino@gmail.com>
2015-07-17 10:46:21 +0200

    Sección créditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,19 @@ var quizController =require("../controllers/quiz_controller");
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+// --> Instalamos un MiddleWare al enrutador
++/* GET author page. */
+router.get("/author", function(req, res) {
+  res.render("author", { 
+						curso: "Desarrollo de servicios en la nube con HTML5, JavaScript y NodeJS",
+						nombre: "Angel Sobrino",  
+						profesion:"Docente programación",
+						profesor:"Juan Quemada",
+						foto:"./images/photo.jpg",
+						titulofoto:"Angel Sobrino"
+						});
+});
 router.get("/quizes/question",quizController.question);
 router.get("/quizes/answer",quizController.answer);
 module.exports = router;
