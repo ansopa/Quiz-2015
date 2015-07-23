b89741fc8bb0ef223f9a42b1d87ab5dbd38ef9e7
ansopa <angel.sobrino@gmail.com>
2015-07-23 18:16:45 +0200

    Lista de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,24 @@
 var models=require("../models/models.js");
 
-//GET /quizes/question
-exports.question = function(req, res){
-models.Quiz.findAll().then(function(quiz){
-	res.render("quizes/question",{pregunta:quiz[0].pregunta});
-})
+
+// GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll(options).then(function(quizes) {
+    		res.render('quizes/index.ejs', {quizes: quizes}); 
+      	})
 };
-//get /quizes/answer
-exports.answer=function(req, res){
-	models.Quiz.findAll().then(function(quiz){
-	if(req.query.respuesta===quiz[0].respuesta){
-		res.render("quizes/answer", {respuesta:"Correcto"});
+
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+	res.render('quizes/show', { quiz: req.quiz});
+};
+
+// GET  /quizes/:id/answer
+exports.answer = function(req, res) {
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
+			resultado = 'Correcto';
 	}
-	else{
-		res.render("quizes/answer", {respuesta:"Incorrecto"});
-	}
-})
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,7 @@ router.get("/author", function(req, res) {
 						titulofoto:"Angel Sobrino"
 						});
 });
-router.get("/quizes/question",quizController.question);
-router.get("/quizes/answer",quizController.answer);
+router.get('/quizes', 					   quizController.index);
+router.get('/quizes/:quizId(\\d+)', 	   quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 module.exports = router;
